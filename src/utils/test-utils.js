1e69e38141c7254bc0048cef6801424601279d0e
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-06-02 00:40:16 +0300

    SS-20 add utilities for testing redux

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../store/index";
 
-export default function renderWithRouter(
+export function renderWithRouter(
   ui,
   {
     route = "/",
@@ -15,6 +17,31 @@ export default function renderWithRouter(
     // adding `history` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
+    history,
+  };
+}
+
+export function renderWithStore(ui) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return render(ui, { wrapper: Wrapper });
+}
+
+export function renderWithRouterAndStore(
+  ui,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {}
+) {
+  return {
+    ...render(
+      <Provider store={store}>
+        <Router history={history}>{ui}</Router>
+      </Provider>
+    ),
+    store,
     history,
   };
 }
