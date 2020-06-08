be64f442a98ee5d00a822ef28132b43d6a8f9f2e
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-06-08 15:59:46 +0300

    SS-21 introduce styled components global styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from "react";
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Page404 from "./pages/Page404";
 import Category from "./pages/Category";
 import Cart from "./pages/Cart";
-import "./utils/utility-classes.css";
 import Checkout from "./pages/Checkout";
 import Product from "./pages/Product";
+
+import "./utils/utility-classes.css";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+  }
+
+  body a {
+    color: black;
+  }
+
+  body a:hover {
+    color: black;
+  }
+`;
 
 function App() {
   return (
     <div className="app">
+      <GlobalStyles />
       <Switch>
         <Route path="/login" component={Login} />
         <Route exact path="/" component={Home} />
