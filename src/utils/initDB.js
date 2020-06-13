2f42a4ace317b3509da2193d4fa180544d34cd87
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-06-13 22:33:36 +0300

    SS-22 add data from json to firebase

---
+++ src/utils/initDB.js
@@ -0,0 +1,61 @@
+import { db } from "../apis/firebase/firebase";
+import json from "./products.json";
+
+function removeId(obj) {
+  const result = {};
+
+  const objKeys = Object.keys(obj);
+
+  objKeys.forEach((key) => {
+    if (key !== "id") {
+      result[key] = obj[key];
+    }
+  });
+
+  return result;
+}
+
+export function deleteDocuments() {
+  db.collection("products")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => doc.ref.delete());
+    });
+  db.collection("categories")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => doc.ref.delete());
+    });
+}
+
+export function initDB() {
+  const categories = Object.keys(json);
+
+  categories.forEach((category) => {
+    const categoryContent = json[category];
+    const itemsIdList = [];
+    const promiseArray = [];
+
+    categoryContent.items.forEach((product) => {
+      const addProductPromise = db
+        .collection("products")
+        .add(removeId(product));
+      promiseArray.push(addProductPromise);
+    });
+
+    // Products are added to firebase async, so we have to wait for
+    // all the promises to resolve in order to get the product ids
+    Promise.all(promiseArray).then((docRefs) => {
+      docRefs.forEach((docRef) => {
+        itemsIdList.push(docRef.id);
+      });
+
+      db.collection("categories")
+        .doc(category)
+        .set({
+          ...categoryContent,
+          items: itemsIdList,
+        });
+    });
+  });
+}
