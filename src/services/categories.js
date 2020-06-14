7f8de68f233389f2a8cef0542a929181e2ed68ae
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-06-14 14:57:47 +0300

    SS-22 add `getCategoryService`

--- src/services/categories.js
+++ src/services/categories.js
@@ -22,3 +22,40 @@ export const getCategoriesService = () =>
       })
       .catch((error) => reject(error));
   });
+
+export const getCategoryService = (categoryName) =>
+  new Promise((resolve, reject) => {
+    db.collection("categories")
+      .doc(categoryName)
+      .get()
+      .then((categoryRef) => {
+        const category = categoryRef.data();
+        const productsArray = [];
+        const promiseArray = [];
+
+        category.items.forEach((itemId) => {
+          const productPromise = db.collection("products").doc(itemId).get();
+          promiseArray.push(productPromise);
+        });
+
+        // first get all the products then add their info
+        Promise.all(promiseArray).then((products) => {
+          products.forEach((productRef) => {
+            const data = productRef.data();
+            productsArray.push({
+              id: productRef.id,
+              name: data.name,
+              price: data.price,
+              currency: data.currency,
+              image: data.image,
+            });
+          });
+
+          resolve({
+            name: category.name,
+            items: productsArray,
+          });
+        });
+      })
+      .catch((error) => reject(error));
+  });
