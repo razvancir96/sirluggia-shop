98201be78ed9d96297fb73faf5a23c3dccea252f
razvancir96 <40989335+razvancir96@users.noreply.github.com>
2020-07-02 22:58:57 +0300

    SS-25 add minimal mocks for services

---
+++ src/services/__mocks__/products.js
@@ -0,0 +1,15 @@
+export const getProductService = () =>
+  Promise.resolve({
+    id: 1,
+    name: "Sneakerși piele Massimo Dutti",
+    price: 135,
+    currency: "LEI",
+    image: "https://i.imgur.com/Rz8lVQa.jpg",
+    size: 45,
+    colour: "Negru",
+    material: "Piele",
+    brand: "Massimo Dutti",
+    description:
+      "Sneakerși din piele cu talpă inițial albă, din colecția de toamnă-iarnă 2019, Massimo Dutti.",
+    quantity: 0,
+  });
